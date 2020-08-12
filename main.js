536fffb2cbbad8204182632ce6d2c4c34b0d8e90
denis.savgir@codeimmersives.com <denis.savgir@codeimmersives.com>
2020-08-12 14:09:40 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// API
+
+const sampleTask = {
+	taskId: 12345,
+	title: "Sample Task",
+	description: "Sample task template to describe the task structure",
+	done: false,
+	startDate: "",
+	startTime: "",
+	dueDate: "08-12-2020",
+	dueTime: ""
+}
+
+// Routes:
+
+// GET /tasks
+// GET /task/:id
+// POST
+// PUT /updatetask/:id
+// DELETE /killtask/:id
